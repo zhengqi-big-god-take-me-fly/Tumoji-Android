1d95e6300cd4fc99ce5e8c48d7e2e743dbb93e94
xiongdajq <wangjiaqi1995@163.com>
2016-12-17 00:53:08 +0800

    can back but the next button can not use.

--- app/src/main/java/com/tumoji/tumoji/account/view/SignInSignUpActivity.java
+++ app/src/main/java/com/tumoji/tumoji/account/view/SignInSignUpActivity.java
@@ -2,8 +2,13 @@ package com.tumoji.tumoji.account.view;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.tumoji.tumoji.R;
@@ -11,33 +16,85 @@ import com.tumoji.tumoji.account.contract.SignInSignUpContract;
 import com.tumoji.tumoji.account.presenter.SignInSignUpPresenter;
 import com.tumoji.tumoji.data.account.repository.MockAccountRepository;
 
-public class SignInSignUpActivity extends AppCompatActivity implements SignInSignUpContract.View {
+import static android.view.View.*;
+
+public class SignInSignUpActivity extends AppCompatActivity implements SignInSignUpContract.View{
     private SignInSignUpContract.Presenter mPresenter;
+    private Button next;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+    FragmentManager fragmentManager = getFragmentManager();
+    android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//
+//    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        next = (Button)findViewById(R.id.signinsignup_next);
+        next.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View v) {
+                fab.setVisibility(VISIBLE);
+                pushSignInSignOutProgress();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_sign_up);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab.setVisibility(INVISIBLE);
+                //fragmentManager.popBackStack();
+                fragmentManager.popBackStackImmediate();
+            }
+        });
+        fab.setVisibility(INVISIBLE);
         setSupportActionBar(toolbar);
-
         mPresenter = new SignInSignUpPresenter(MockAccountRepository.getInstance(), this);
         mPresenter.init();
-
-        FragmentManager fragmentManager = getFragmentManager();
-        android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        SignInSignUpFragment.SignInSignUpExample fragement = new SignInSignUpFragment.SignInSignUpExample();
+        SignInSignUpExample fragement = new SignInSignUpExample();
         fragmentTransaction.add(R.id.fragment_container, fragement);
         fragmentTransaction.commit();
+        if (next != null) {
+            Log.v("====", "next==========");
+            next.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    fab.setVisibility(VISIBLE);
+                    pushSignInSignOutProgress();
+                }
+            });
+        } else {
+            Log.v("next", "=====fuck");
+        }
     }
+
+
 
     @Override
     public void pushSignInSignOutProgress() {
         // TODO: Implement com.tumoji.tumoji.account.view.SignInSignUpActivity.pushSignInSignOutProgress
-
-        throw new UnsupportedOperationException("Method not implemented");
+        Log.v("000", "=================1");
+        android.app.FragmentTransaction fragmentTransaction1 = fragmentManager.beginTransaction();
+        Log.v("000", "=================2");
+        SignInSignUpFragment.SignInExample fragment1 = new SignInSignUpFragment.SignInExample();
+        fragmentTransaction1.replace(R.id.fragment_container, fragment1);
+        fragmentTransaction1.addToBackStack("SignInSignUp");
+        Log.v("000", "=================5");
+        fragmentTransaction1.commit();
+        //throw new UnsupportedOperationException("Method not implemented");
     }
 
     @Override
