548624293d126bb1bd67edc93bb892bfa4d1bd1b
Souler Ou <ou@souler.me>
2016-12-17 17:21:08 +0800

    Implement ACCOUNTAPI

--- app/src/main/java/com/tumoji/tumoji/network/retrofit/AccountAPI.java
+++ app/src/main/java/com/tumoji/tumoji/network/retrofit/AccountAPI.java
@@ -1,12 +1,18 @@
 package com.tumoji.tumoji.network.retrofit;
 
 import com.tumoji.tumoji.data.account.model.AccountModel;
+import com.tumoji.tumoji.data.meme.model.MemeModel;
+import com.tumoji.tumoji.utils.AccountRole;
+import com.tumoji.tumoji.utils.BooleanResponse;
 import com.tumoji.tumoji.utils.ErrorType;
+import com.tumoji.tumoji.utils.IntegerResponse;
 import com.tumoji.tumoji.utils.Token;
 
 import java.util.List;
 
 import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.Field;
@@ -80,4 +86,77 @@ public interface AccountAPI {
 
     @DELETE("/users/{id}/accessTokens")
     Observable<ErrorType> removeAllAccessTokensByUserId(@Path("id") String id, @Field("access_token") String token);
+
+    @GET("/users/{id}/accessTokens/count")
+    Observable<IntegerResponse> checkAccessTokenCountByUserId(@Path("id") String id);
+
+    @GET("/users/{id}/exists")
+    Observable<BooleanResponse> checkUserExistenceByUserId(@Path("id") String id);
+
+    @GET("/users/{id}/expressions")
+    Observable<List<MemeModel>> getExpressionsPostedByUserId(@Path("id") String id);
+
+    /**
+     * Need to PUT JSON Here
+     * Need to initialized by these steps:
+     * -> Gson gson = new Gson();
+     * -> String route = gson.toJson(memeModel);
+     * -> RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"),route);
+     * Then we can put body when calling this methods
+     * @param route
+     * @return
+     */
+    @Headers({"Content-Type: application/json" , "Accept: application/json"})
+    @POST("/users/{id}/expressions")
+    Observable<MemeModel> addNewExpressionByUser(@Path("id") String id , @Body RequestBody route , @Field("access_token") String token);
+
+    @DELETE("/users/{id}/expressions")
+    Observable<ErrorType> removeAllExpressionsByUserId(@Path("id") String id);
+
+    @GET("/users/{id}/expressions/count")
+    Observable<IntegerResponse> getUserExpressionCountById(@Path("id") String id);
+
+    @GET("/users/{id}/likes")
+    Observable<List<MemeModel>> getAllLikedMemesByUserId(@Path("id") String id , @Field("access_token") String token);
+
+    @GET("/users/{id}/likes/count")
+    Observable<IntegerResponse> getAllLikedMemesCountByUserId(@Path("id") String id , @Field("access_token") String token);
+
+    /**
+     * Need to PUT JSON Here
+     * Need to initialized by these steps:
+     * -> Gson gson = new Gson();
+     * -> String route = gson.toJson(accountModel);
+     * -> RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"),route);
+     * Then we can put body when calling this methods
+     * @param route
+     * @return
+     */
+    @Headers({"Content-Type: application/json" , "Accept: application/json"})
+    @POST("/users/{id}/replace")
+    Observable<AccountModel> replaceUserById(@Path("id") String id , @Body ResponseBody route , @Field("access_token") String token);
+
+    @GET("/users/{id}/roles")
+    Observable<AccountRole> getUserRoleById(@Path("id") String id , @Field("access_token") String token);
+
+
+    /**
+     * Need to PUT JSON Here
+     * Need to initialized by these steps:
+     * -> Gson gson = new Gson();
+     * -> String route = gson.toJson(accountLoginModel);
+     * -> RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"),route);
+     * Then we can put body when calling this methods
+     * @param route
+     * @return
+     */
+    @Headers({"Content-Type: application/json" , "Accept: application/json"})
+    @POST("/users/login")
+    Observable<Token> requestLogin(@Body ResponseBody route);
+
+
+    @POST("/users/logout")
+    Observable<ErrorType> requstLogout(@Field("access_token") String token);
+
+
 }
